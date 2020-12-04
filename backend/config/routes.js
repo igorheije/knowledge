ef915a7f1d1ca4c43083b7323dbe933f18c4801b
Igor Heije <igorheije85@gmail.com>
2020-12-04 17:49:58 -0300

    desenvolvendo api

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,48 @@
+const admin = require('./admin.js')
+
 module.exports = app =>{
+  app.post('/signup', app.api.user.save)
+  app.post('/signin', app.api.auth.signin)
+  app.post('/validateToken', app.api.auth.validateToken)
+
   app.route('/users')
-    .post(app.api.user.save)
+    .all(app.config.passport.authenticate())
+    .post(admin(app.api.user.save))
+    .get(admin(app.api.user.get))
+
+  app.route('/users/:id')
+    .all(app.config.passport.authenticate())
+    .put(admin(app.api.user.save))
+    .get(admin(app.api.user.getById))
+
+  app.route('/categories')
+    .all(app.config.passport.authenticate())
+    .get(admin(app.api.category.get))
+    .post(admin(app.api.category.save))
+
+  app.route('/categories/tree')
+    .all(app.config.passport.authenticate())  
+    .get(app.api.category.getTree)
+
+  app.route('/categories/:id')
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getById)
+    .put(admin(app.api.category.save))
+    .delete(admin(app.api.category.remove))
+
+  app.route('/articles')
+    .all(app.config.passport.authenticate())
+    .get(admin(app.api.article.get))
+    .post(admin(app.api.article.save))
+
+  app.route('/articles/:id')
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.get)
+    .put(admin(app.api.article.save))
+    .delete(admin(app.api.article.remove))
+
+  app.route('/categories/:id/articles')
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.getByCategory)
+
 }
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const db = require('./config/db')
 app.db = db
 
 consign()
+  .include('./config/passport.js')
   .then('./config/middlewares.js')
+  .then('./api/validation.js')
   .then('./api')
   .then('./config/routes.js')
   .into(app)
