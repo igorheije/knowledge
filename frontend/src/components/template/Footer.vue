c64c9ac3bc04009f94d38e184ae28d9f04ce7984
Igor Heije <igorheije85@gmail.com>
2020-12-05 14:42:45 -0300

    iniciando frontend

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <span>PRODUZIDO POR IGOR</span>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer'
+
+}
+</script>
+
+<style>
+  .footer{
+    grid-area: footer;
+    background-color: #ddd;
+    color: #333;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 10px;
+  }
+</style>
