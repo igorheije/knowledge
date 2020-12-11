54782be643ac9ebd4601c18430853b408e60627b
Igor Heije <heije.tomori2014@outlook.com>
2020-12-11 19:22:15 -0300

    admin

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -2,16 +2,34 @@
   <div class="admin-pages">
     <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
     sub="Cadastros & Cia"/>
+    <div class="admin-page-tabs">
+      <b-card no-body>
+          <b-tabs card>
+            <b-tab title="Artigos" active>
+              <ArticlesAdmin/>
+            </b-tab>
+            <b-tab title="categorias">
+              <CategoryAdmin/>
+            </b-tab>
+            <b-tab title="Usuários">
+              <UserAdmin/>
+            </b-tab>
+          </b-tabs>
+      </b-card>
+    </div>
 
   </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ArticlesAdmin from './ArticlesAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
 
 export default {
   name: "AdminPages",
-  components: {PageTitle}
+  components: {PageTitle,CategoryAdmin, UserAdmin,ArticlesAdmin}
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,17 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msg'
 import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
+//TEporarios
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Iklnb3IgSGVpamUgTS4iLCJlbWFpbCI6ImhlaWplQGhvdG1haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYwNzM1OTM5MCwiZXhwIjoxNjA3NDQ1NzkwfQ.8pluxKmR6fNdUw4Sek9mNCRJziBMixbjpbrz7GOoYWY'
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
